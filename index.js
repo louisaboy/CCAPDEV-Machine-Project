0bef573f444e3b79ac53f8baf259a7e677139e3b
louisaboy <louis_aboy@dlsu.edu.ph>
2021-05-24 21:31:21 +0800

    additional commit

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-const userRoute = require('./route/user');
-app.use("/user", userRoute);
-app.listen(3000, () => {
-    console.log("Server running on port 3005")
-});
